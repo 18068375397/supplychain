04332d8c1d83dea7c62c83b3a18419a177c4e4cc
seven <1149378768@qq.com>
2018-07-11 14:33:03 +0800

    代码调整

--- supplychain-web/src/main/java/com/xinyonghang/supplychain/controller/FileController.java
+++ supplychain-web/src/main/java/com/xinyonghang/supplychain/controller/FileController.java
@@ -22,12 +22,9 @@ public class FileController {
 
     /**
      * 普通页面的上传图片
-     *
-     * @param file
-     * @param req
-     * @param response
-     * @return
-     * @throws Exception
+     * @param file 上传图片
+     * @return 上传结果
+     * @throws Exception 异常
      */
     @RequestMapping(value = {"/upload"}, method = {RequestMethod.POST})
     @ResponseBody
@@ -36,7 +33,7 @@ public class FileController {
         File tempFile = new File(basePath + fileName);
         // 如果目录不存在就先创建目录
         if (!tempFile.getParentFile().exists()) {
-            tempFile.getParentFile().mkdirs();
+                tempFile.getParentFile().mkdirs();
         }
         if (!file.isEmpty()) {
             BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(tempFile));
@@ -49,11 +46,10 @@ public class FileController {
 
     /**
      * Ckeditor 上传图片  需要会掉ck的callback方法
-     *
-     * @param file
-     * @param req
-     * @param response
-     * @throws Exception
+     * @param file 图片文件
+     * @param req 请求
+     * @param response  响应
+     * @throws Exception 异常
      */
     @RequestMapping(value = {"/uploadCkeditor"}, method = {RequestMethod.POST})
     @ResponseBody
@@ -62,7 +58,7 @@ public class FileController {
         File tempFile = new File(basePath + fileName);
         // 如果目录不存在就先创建目录
         if (!tempFile.getParentFile().exists()) {
-            tempFile.getParentFile().mkdirs();
+                tempFile.getParentFile().mkdirs();
         }
         if (!file.isEmpty()) {
             // 保存图片到服务器
@@ -75,10 +71,9 @@ public class FileController {
             OutputStream outputStream = response.getOutputStream();
             String rootURL = req.getScheme() + "://" + req.getServerName() + ":" + req.getServerPort() + req.getContextPath();
             // 返回"图像"选项卡并显示图片 request.getContextPath()为web项目名
-            String callback = req.getParameter("CKEditorFuncNum").toString();
-            StringBuffer stringBuffer = new StringBuffer();
-            stringBuffer.append("<script type=\"text/javascript\">").append("window.parent.CKEDITOR.tools.callFunction(").append(callback + ",'" + rootURL).append("/getImage?imagePath=" + fileName + "','')").append("</script>");
-            byte[] b = stringBuffer.toString().getBytes("utf-8");
+            String callback = req.getParameter("CKEditorFuncNum");
+            byte[] b;
+            b = ("<script type=\"text/javascript\">" + "window.parent.CKEDITOR.tools.callFunction(" + callback + ",'" + rootURL + "/getImage?imagePath=" + fileName + "','')" + "</script>").getBytes("utf-8");
             outputStream.write(b, 0, b.length);
             outputStream.flush();
 
@@ -99,25 +94,28 @@ public class FileController {
                 os = response.getOutputStream();
 
                 byte[] buf = new byte[1024 * 100];
-                int position = 0;
+                int position;
                 while ((position = fis.read(buf)) != -1) {
                     os.write(buf, 0, position);
                 }
                 os.flush();
             }
         } catch (Exception e) {
+            e.printStackTrace();
         } finally {
             try {
                 if (os != null) {
                     os.close();
                 }
             } catch (IOException e) {
+                e.printStackTrace();
             }
             try {
                 if (fis != null) {
                     fis.close();
                 }
             } catch (IOException e) {
+                e.printStackTrace();
             }
         }
 
